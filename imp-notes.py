bd2ab71179946d0ff18f8b646b752373ad488f3d
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-04-01 14:47:22 +0530

    These are some important  points to remember

---
+++ imp-notes.py
@@ -0,0 +1,76 @@
+"""
+Variable Names-
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+
+Rules for Python variables:
+
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords.
+
+
+Multi Words Variable Names-
+Variable names with more than one word can be difficult to read.
+
+There are several techniques you can use to make them more readable:
+
+Camel Case
+Each word, except the first, starts with a capital letter:
+
+myVariableName = "John"
+Pascal Case
+Each word starts with a capital letter:
+
+MyVariableName = "John"
+Snake Case
+Each word is separated by an underscore character:
+
+my_variable_name = "John"
+
+
+
+
+Python has the following data types built-in by default, in these categories:
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+
+
+
+
+
+
+
+
+
+
+
+
+
+Example	                                                               Data Type	
+x = "Hello World"	                                                      str	
+x = 20	                                                                  int	
+x = 20.5	                                                             float	
+x = 1j                                                                 	complex	
+x = ["apple", "banana", "cherry"]	                                      list	
+x = ("apple", "banana", "cherry")	                                      tuple	
+x = range(6)	                                                          range	
+x = {"name" : "John", "age" : 36}                                       	dict	
+x = {"apple", "banana", "cherry"}	                                        set	
+x = frozenset({"apple", "banana", "cherry"})	                         frozenset	
+x = True	                                                                bool	
+x = b"Hello"	                                                           bytes	
+x = bytearray(5)	                                                       bytearray	
+x = memoryview(bytes(5))                                                memoryview	
+x = None	                                                             NoneType	
+
+
+"""
