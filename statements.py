cfac9f0fba61e1da3b2d0af747b702e5bc498fdb
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-04-03 21:28:15 +0530

    Print multiple statements

---
+++ statements.py
@@ -0,0 +1,8 @@
+print("This is the first statement") 
+print("This is the second statement")
+print("This is the third statement")
+
+# ye teeno sentence ek ek krke line se print ho jayge 
+# pr agar hum chahte h ki teeno statement ek hi line m likhe but print teen lines m hi ho to fir.........ase likhege..
+
+print("This is an statement"); print("This is an statement"); print("This is an statement"); 
