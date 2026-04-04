c6a7ca00245651e4a9dc1019f4bf3a79b4fc203a
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-04-04 08:55:09 +0530

    Print the string in upper and lower casee

---
+++ upper.py
@@ -0,0 +1,12 @@
+a='hello'
+print(a.upper())
+
+# upper se sare characters uppercase m aa jate h
+
+a='HELlo'
+print(a.lower())
+
+# lower se sare characters lowercase m a jate h 
+
+a='hello  guyzz'
+print(a.strip())
