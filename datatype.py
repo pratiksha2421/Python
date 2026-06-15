febfbfe2438555ec113d48463e6bb3effa4d1c87
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:42:09 +0530

    assign data type

---
+++ datatype.py
@@ -0,0 +1,9 @@
+x= int(5)
+y= str('rimjhim')
+z= float(8)
+print(x)
+print(y)
+print(z)
+
+# isme variable create krne ke liye value assign krni pddti h to unke data type bhi just assign krne se phle likhna pddta h hme
+# string variables ko hum sinngle or double quotes dono se declare kr skte h
