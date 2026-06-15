e2ecb4d760acd875c3f265e49c1c87ffff52b2d1
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:52:46 +0530

    we gave ka particular name and stores value in it

---
+++ unpack.py
@@ -0,0 +1,8 @@
+colors=["black", "red", "pink", "blue"]
+w, x, y, z = colors
+print(w)
+print(x)
+print(y)
+print(z)
+
+# isme humne ek common name type le liya or usme sari values daldi or print usko print kra diya
