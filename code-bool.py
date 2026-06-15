02041560bf796cf95eb73a27c79c3baa41b8c23f
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:39:47 +0530

    print the result

---
+++ code-bool.py
@@ -0,0 +1,9 @@
+# Print the result of 10 > 9
+# Print the result of 10 == 9
+# Print the result of bool("Hello")
+# Print the result of bool(0)
+
+print(10>9)
+print(10==9)
+print(bool("Hello"))
+print(bool(0))
