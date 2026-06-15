75bf93c69d728f1a09772bcc8c5b20e21e4e3cdb
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:50:39 +0530

    multiple values assign

---
+++ multiplevalues.py
@@ -0,0 +1,13 @@
+x,y,z=5,6,7
+print(x)
+print(y)
+print(z)
+
+# ek hi line m ek se jada variable ke liye value assign kr skte h
+
+x=y=z=8
+print(x)
+print(y)
+print(z)
+
+# ek hi value ko ek se jada variable ke liye bhi assign kr skte h
