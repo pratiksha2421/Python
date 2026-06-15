5d6b1c4597051fc0666f3bf6986e76e6d668fc7a
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:33:32 +0530

    arithmetic statements

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+x=6
+y=7
+print(x+y)  # + se do variable add hoge
+print(x-y)  # - e do variable subtract hoge
+print(x*y)  # * se do variable multiply hote h
+print(x/y)  # / se do variable divide hote h it returns float value
+print(x%y)  # % se do variable ka remainder jo hoga vo ajayga
+print(x**y) # ** se jese agar 6 or 7 h values to 6 ko 7 br multiply kr dege like 6*6*6*6*6*6*6
+print(x//y) # // isse jb hum koi do numbers ko divide krege or answer agar point m hoga to point ke bd vali values ni lege and it return int value
