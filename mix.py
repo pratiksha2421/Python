4f2916a969312266d5d60a38e9b63b6acb8aec8e
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:47:29 +0530

    we can print both variable and statement

---
+++ mix.py
@@ -0,0 +1,4 @@
+print('I am' ,20 ,'years old')  # hum numbers or text ko mix krke bhi likh skte h ase....text ko quotes m or number ko without quotes pr text ke bd comma jaruri h vrna error ayga number m
+
+print('I am 20 years old') # vese hum ye ase bhi likh skte thai............
+
