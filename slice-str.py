cac580b154fe8a3aa1348ea544749fc6918bc0bc
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:51:52 +0530

    we can decide in which place we have to place string

---
+++ slice-str.py
@@ -0,0 +1,14 @@
+a='hello'
+print(a[1:5])
+
+# isse hme kitni range pr print krwana h string ke characters vo decide kr skte h hum 
+
+a='hello'
+print(a[:5])
+
+# agar hmne sirf end vali range likhi h or colon lgaya start m to fir vo start ka apne app le lega to jha se start ho rha hoga vha se print hoge characters 
+
+a='hello'
+print(a[4:])
+
+# isme : ke phle start range dedi to end vali khud le lega mtlb jha tk characters hoge print krwa dega
