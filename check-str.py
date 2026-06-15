b7569debafbedb65fff0ccc08a8b880e1e5bebb2
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:39:04 +0530

    check if string is there or not

---
+++ check-str.py
@@ -0,0 +1,25 @@
+a='This is a sentence'
+print('is' in a)
+
+# agar hme check krna h koi word string m h ya ni to hum in ka use krke print m check kr skte h.....isko hum if se bhi bhi kr skte h as given below..
+
+
+a='This is the second statement'
+if('second' in a):
+ print("Yes , second is present in the statement")
+
+
+ # or agar hme ye check krna h ki koi word uss string m ni h kya to vha not in ka use hoga.. as given below..
+
+
+
+ a='Hey guyzz , i am learning python'
+ print('Hello' not in a)
+
+
+# isko bhi hum if se kr skte h as given below..
+
+
+ a='this is a another statement'
+ if('hello' not in a):
+  print('hello is not in the statement')
