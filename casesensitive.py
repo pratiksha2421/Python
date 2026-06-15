2f107b5f45f44104d192da11e43f3ed10b5e7814
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:38:18 +0530

    case-sensitive

---
+++ casesensitive.py
@@ -0,0 +1,6 @@
+a=5
+A=7
+print(a)
+print(A)
+
+# variables ke jo name hum rkhte h vo case sensitive hote h like small a jo value hold kr rha h vo alg h or capital A jo kr rha vo alg hoga overright ni hoga
