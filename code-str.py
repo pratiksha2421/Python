21edb50a605b7c8cb4c5860c593914f9a45b0bd8
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:40:08 +0530

    print the result

---
+++ code-str.py
@@ -0,0 +1,12 @@
+# Create a variable txt with the value "Hello, World!" 
+# Print the characters from index 2 to 5 (slicing) 
+# Print txt converted to upper case
+# Create a variable name with the value "Python"
+# Use an f-string to print "I love Python" using the name variable
+
+txt= "Hello, World!"
+print(txt[2:5])
+print(txt.upper())
+a="Python"
+b= f"I love {a}"
+print(b)
