d4418d1a511abad1be96d5d2f3afdfbade70eb58
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-06-15 21:36:14 +0530

    bool returns value either true or false

---
+++ bool.py
@@ -0,0 +1,6 @@
+print(9>10)
+print(9==9)
+
+# jb hum koi condition statement run krayge tb hme true or false m value return hogi
+
+# bool m mostly value vo true hi return krega except ki koi value empty ya none ya fir 0 ho tb uss condition m fir vo false return krta h 
